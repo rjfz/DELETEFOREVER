37643e14384905e5cd1ad41d3edcd14bb61f8b5e
rjfzgit config --global user.name rjfzrails server -b 0.0.0.0git branch -M mainudi-dev:~/blog$ git config --global user.name rjfz <rjsfeeney@gmail.com>
2021-01-17 14:32:07 +0000

    added images to users, formatted articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :logged_in?
+  helper_method :require_login
+  helper_method :author_permission
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = 'You must be logged in to access this section'
+      redirect_to '/signin'
+    end
+  end
 end
